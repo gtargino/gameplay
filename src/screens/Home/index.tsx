7411582ff31b165fad216382f42eaf4df3312309
unknown <gustavodiniztargino@hotmail.com>
2021-08-21 01:06:02 -0300

    appointments

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { styles } from './styles';
 
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
 import ListHeader from '../../components/ListHeader';
-
+import { Appointment } from '../../components/Appointment';
 
 export function Home() {
     const [ category, setCategory ] = useState('');
+
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '20/08/2021',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    
+        }
+    ]
 
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -29,6 +45,13 @@ export function Home() {
                 <ListHeader
                     title='Partidas agendadas'
                     subtitle='Total 6'
+                />
+                <FlatList
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    renderItem= {({ item }) => (
+                        <Appointment data={item} />
+                    )}
                 />
             </View>
         </View>
