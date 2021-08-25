9c35b58b5dd39b81324d786fd43c604ade73a913
unknown <gustavodiniztargino@hotmail.com>
2021-08-24 23:47:22 -0300

    appointmentsfix

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
+import PlayerSvg from '../../assets/player.svg';
 import { GuildIcon } from '../GuildIcon';
+import { theme } from '../../global/styles/theme';
+import { categories } from '../../utils/categories';
 
 export type GuildProps = {
-    
+    owner: true
 }
 
 export type AppointmentProps = {
@@ -22,10 +25,27 @@ export type Props = RectButtonProps & {
 }
 
 export function Appointment({data, ...rest}: Props) {
+    const [category] = categories.filter(item => item.id === data.category);
+    const { owner } = data.guild;
+    const { primary, on } = theme.colors;
+
     return(
         <RectButton {...rest}>
             <View style={styles.container}>
                 <GuildIcon />
+                <View style={styles.content}>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>
+                            { data.guild.name }
+                        </Text>
+                        <Text style={styles.category}>
+                            { category.title }
+                        </Text>
+                    </View>
+                    <View style={styles.playersInfo}>
+                        <PlayerSvg fill={ owner ? primary : on } />
+                    </View>
+                </View>
             </View>
         </RectButton>
     )
