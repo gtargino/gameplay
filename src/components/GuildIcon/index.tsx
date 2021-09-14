50ba552f528f62fb869ebb70c578804d0ac8a3ef
unknown <gustavodiniztargino@hotmail.com>
2021-09-14 00:17:47 -0300

    iconsfromAPI

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { styles } from './styles';
 
-export function GuildIcon() {
-    const uri = 'https://steamuserimages-a.akamaihd.net/ugc/952964242161767666/A0686A3BAC68404FF93C9BEDD52E52436B8E3199/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false';
+import { View, Image } from 'react-native';
+import { styles } from './styles';
+import DiscordSvg from '../../assets/discord.svg'
+
+const { CDN_IMAGE } = process.env;
+
+type Props = {
+    guildId: string;
+    iconId: null | string;
+}
+
+export function GuildIcon({ guildId, iconId}: Props) {
+    //const uri = 'https://steamuserimages-a.akamaihd.net/ugc/952964242161767666/A0686A3BAC68404FF93C9BEDD52E52436B8E3199/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false';
     //const uri = 'https://img.ibxk.com.br/2020/10/30/30110741964176.jpg?w=352&h=208&mode=crop&scale=both';
+    const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
     
     return(
-        <Image
-            source={{ uri }}
-            style={styles.image}
-            resizeMode="cover"
-        />
+        <View style={styles.container}>
+            {
+                iconId ?
+                <Image
+                    source={{ uri }}
+                    style={ styles.image }
+                    resizeMode="cover"
+                />
+                :
+                <DiscordSvg
+                    width={40}
+                    height={40}
+                />
+            }
+        </View>
     )
 }
--- src/components/GuildIcon/styles.ts
+++ src/components/GuildIcon/styles.ts
@@ -2,9 +2,17 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
-    image: {
+    container: {
         height: 62,
         width: 66,
-        borderRadius: 8
+        borderRadius: 8,
+        backgroundColor: theme.colors.discord,
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden'
+    },
+    image: {
+        height: 62,
+        width: 66        
     }
 });
